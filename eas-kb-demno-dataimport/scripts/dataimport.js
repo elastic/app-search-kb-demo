c9850f623b6396076c7fd0e030174918f7fc2f72
Aurélien FOUCRET <aurelien.foucret@elastic.co>
2020-03-09 08:44:30 +0100

    Helpdesk data import.

---
+++ eas-kb-demno-dataimport/scripts/dataimport.js
@@ -0,0 +1,104 @@
+const prompts = require('prompts')
+const fs = require('fs')
+const AppSearchClient = require('@elastic/app-search-node')
+const cliProgress = require('cli-progress');
+const moment = require('moment');
+
+const BATCH_SIZE          = 100;
+const DATA_DIR_NAME       = process.cwd() + '/data';
+const DEFAULT_URL         = 'http://localhost:3002/api/as/v1/';
+const META_ENGINE_NAME    = 'helpdesk';
+const PROGRESS_BAR_CONFIG = {
+  clearOnComplete: false, 
+  hideCursor: true,
+  format: '  Importing {filename} [{bar}] {percentage}% | ETA: {eta}s | {value}/{total}'
+};
+
+const getClient = () => {
+  return prompts(
+    [
+      {
+        type: 'text',
+        name: 'appSearchUrl',
+        message: 'App Search Server URL:',
+        initial: process.env.APP_SEARCH_URL || DEFAULT_URL
+      },
+      {
+        type: 'text',
+        name: 'apiKey',
+        message: 'App Search Private API key:',
+        initial: process.env.APP_SEARCH_PRIVATE_KEY
+      }
+    ]
+  ).then(({apiKey, appSearchUrl}) => {
+    return Promise.resolve(new AppSearchClient(undefined, apiKey, () => appSearchUrl));
+  });
+};
+
+const getImportedFiles = () => {
+  return new Promise((resolve, reject) => {
+    return fs.readdir(DATA_DIR_NAME, (err, files) => {
+      return err != null ? reject(err) : resolve(files.map(filename => {
+        return {filename: DATA_DIR_NAME + '/' + filename, engine: filename.split('.')[0]};
+      }));
+    });
+  });
+};
+
+const processDoc = (doc) => {
+    if (doc.category) {
+      doc.product_name = doc.category;
+      doc.product_name_raw = doc.category;
+      delete doc.category;
+    }
+
+    if (doc.date) {
+      doc.date = moment(doc.date.match(/(.*?(am|pm))/)[0], 'MMMM DD, YYYY, H:mma').format();
+    } 
+    
+    if (doc.published_at) {
+      doc.date = doc.published_at;
+      delete doc.published_at;
+    }
+
+    return doc;
+};
+
+const importFile = (client, filename, engine, progressBar) => {
+  return client.getEngine(engine)
+    .catch(error => error.message = "Not Found" ? client.createEngine(engine) : Promise.reject(error))
+    .then(async () => {
+      const jsonData = require(filename);
+      const fileProgressBar = progressBar.create(jsonData.length, 0);
+      fileProgressBar.update(0, {filename: filename.padEnd(80)});
+      for (var i = 0; i < jsonData.length; i += BATCH_SIZE) {
+        var docs = jsonData.slice(i, i + BATCH_SIZE).map(processDoc);
+
+        await client.indexDocuments(engine, docs);
+        fileProgressBar.increment(docs.length);
+      }
+
+      return engine;
+    })
+}
+
+getClient().then(client => {
+  const progressBar = new cliProgress.MultiBar({PROGRESS_BAR_CONFIG}, cliProgress.Presets.shades_grey);
+
+  getImportedFiles().then(files => {
+    return Promise.all(files.map(({filename, engine}) => importFile(client, filename, engine, progressBar)));
+  }).then(engines => {
+    progressBar.stop();
+    return client.getEngine(META_ENGINE_NAME)
+      .then(() => {
+        return client.addMetaEngineSources(META_ENGINE_NAME, engines).then(() => {
+          console.log(`✔ Added source engines to the ${META_ENGINE_NAME} meta engine.`);
+        })
+      })
+      .catch(() => {
+        client.createMetaEngine(META_ENGINE_NAME, engines).then(() => {
+          console.log(`✔ Created the ${META_ENGINE_NAME} meta engine.`);
+        });
+      });
+  });
+});
