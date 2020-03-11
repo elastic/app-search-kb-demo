e70a093f4eb53d011ec9ea22da9965839d2f3c31
Aurélien FOUCRET <aurelien.foucret@elastic.co>
2020-03-11 13:28:55 +0100

    Improved readability.

--- eas-kb-demo-frontend/src/Contact/Callout.js
+++ eas-kb-demo-frontend/src/Contact/Callout.js
@@ -8,8 +8,7 @@ class Callout extends React.Component {
     return <div className="app-contact-callout">
       <p className="app-contact-callout-title">Can’t find what you’re looking for?</p>
       <p className="app-contact-callout-content">
-        <span>We’re here to help. Get in touch and we’ll get back to you as soon as possible.</span>
-        <a href="">Contact Support →</a>
+        We’re here to help. Get in touch and we’ll get back to you as soon as possible. <a href="">Contact Support →</a>
       </p>
     </div>
   }
